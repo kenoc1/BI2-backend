d82aae51191943c8939ec1573877312e30c19b97
Felix Dick <felix.dick@btc-ag.com>
2022-02-24 11:05:17 +0100

    new urls and views

--- customer/urls.py
+++ customer/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from order import views
+from customer import views
 
 urlpatterns = [
-    path('checkout/', views.checkout),
+    path('user/login/', views.login),
 ]
--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.http import Http404
+from django.shortcuts import render
+
+from rest_framework import status, authentication, permissions
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+@api_view(['POST'])
+def login(request):
+    print("dffae")
+    print(request.data)
